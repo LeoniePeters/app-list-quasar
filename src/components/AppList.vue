2ce328cbbbf3ff441f1e913269b2490dd68d3bcb
LeoniePeters <leoniepeters@gmail.com>
2019-04-07 10:31:10 +0200

    Create AddList.vue card component

---
+++ src/components/AppList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    My component
+    <q-card inline style="width: 300px">
+      <q-card-title>Card Title</q-card-title>
+      <q-card-separator/>
+      <q-card-main>Card Content</q-card-main>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppList'
+  // data() {
+  //   return {};
+  // }
+}
+</script>
+
+<style>
+</style>
